SlimRoms/device_motorola_shamu

swap: add 1Gb zswap
Change-Id: I89c9298f1d8f769a193207920bfb2be862bb32ae
Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>

device.mk
@@ -100,6 +100,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     device/motorola/shamu/spn-conf.xml:system/etc/spn-conf.xml
 
+# Add swap activation script (workaround for the time being)
+PRODUCT_COPY_FILES += \
+    device/motorola/shamu/swapon.sh:system/xbin/swapon.sh
+
 # This device is 560dpi.  However the platform doesn't
 # currently contain all of the bitmaps at 560dpi density so
 # we do this little trick to fall back to the xxhdpi version

TurboROM-Devices/device_moto_shamu

Conflicts:
	init.shamu.power.rc

device.mk
@@ -101,6 +101,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     device/moto/shamu/spn-conf.xml:system/etc/spn-conf.xml
 
+# Add swap activation script (workaround for the time being)
+PRODUCT_COPY_FILES += \
+    device/moto/shamu/swapon.sh:system/xbin/swapon.sh
+
 # This device is 560dpi.  However the platform doesn't
 # currently contain all of the bitmaps at 560dpi density so
 # we do this little trick to fall back to the xxhdpi version